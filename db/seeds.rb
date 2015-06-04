e2955135d1f0027ee4378f759f427dae0ddef329
Katye Russell <katye.russell@gmail.com>
2015-06-04 14:50:31 -0500

    Initial commit with addition of rspec.

---
+++ db/seeds.rb
@@ -0,0 +1,14 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
+#
+# Examples:
+#
+#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
+#   Mayor.create(name: 'Emanuel', city: cities.first)
+# Student.create(name: 'Tim')
+# Student.create(name: 'Barney')
+
+#  At DB setup or DB create, the seeds get added
+#  Also happens at migration:  rake db:migrate RAILS_ENV=development
+#  rake db:drop RAILS_ENV=development
+#  rake db:create RAILS_ENV=development
